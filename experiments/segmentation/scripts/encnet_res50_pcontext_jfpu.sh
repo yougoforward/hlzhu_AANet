2dd96f2d1743ab171ec369d5c91279e2d9f5a7f7
Hailong Zhu <908865817@qq.com>
2019-04-07 16:52:33 +0400

    resnet

--- experiments/segmentation/scripts/encnet_res50_pcontext_jfpu.sh
+++ experiments/segmentation/scripts/encnet_res50_pcontext_jfpu.sh
@@ -3,30 +3,30 @@
 #train
 python train.py --dataset pcontext \
     --model encnet --jpu --aux --se-loss \
-    --backbone resnet50 --checkname encnet_res50_pcontext
+    --backbone resnet50 --checkname encnet_res50_pcontext_jfpu
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model encnet --jpu --aux --se-loss \
-    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext/model_best.pth.tar \
+    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext_jfpu/model_best.pth.tar \
     --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model encnet --jpu --aux --se-loss \
-    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext/model_best.pth.tar \
+    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext_jfpu/model_best.pth.tar \
      --split val --mode testval --ms
 
 #predict [single-scale]
 python test.py --dataset pcontext \
     --model encnet --jpu --aux --se-loss \
-    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext/model_best.pth.tar \
+    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext_jfpu/model_best.pth.tar \
      --split val --mode test
 
 #predict [multi-scale]
 python test.py --dataset pcontext \
     --model encnet --jpu --aux --se-loss \
-    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext/model_best.pth.tar \
+    --backbone resnet50 --resume runs/pcontext/encnet/encnet_res50_pcontext_jfpu/model_best.pth.tar \
      --split val --mode test --ms
 
 #fps
