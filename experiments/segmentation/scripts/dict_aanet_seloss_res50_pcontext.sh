b27856af8d08464ab9f93a934deae3cc3f72f195
Hailong Zhu <908865817@qq.com>
2019-04-10 17:25:56 +0400

    resnet

--- experiments/segmentation/scripts/dict_aanet_seloss_res50_pcontext.sh
+++ experiments/segmentation/scripts/dict_aanet_seloss_res50_pcontext.sh
@@ -2,11 +2,11 @@
 
 #train
 python train.py --dataset pcontext \
-    --model dict_aanet --aux --jpu --se-loss --base-size 608 --crop-size 576 \
-    --backbone resnet50 --checkname dict_aanet_seloss_jpu__res50_pcontext
+    --model dict_aanet --aux --dilated --se-loss --base-size 608 --crop-size 576 \
+    --backbone resnet50 --checkname dict_aanet_seloss_res50_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model dict_aanet --aux --jpu --se-loss --base-size 608 --crop-size 576 \
-    --backbone resnet50 --resume runs/pcontext/dict_aanet/dict_aanet_seloss_jpu__res50_pcontext/model_best.pth.tar \
+    --model dict_aanet --aux --dilated --se-loss --base-size 608 --crop-size 576 \
+    --backbone resnet50 --resume runs/pcontext/dict_aanet/dict_aanet_seloss_res50_pcontext/model_best.pth.tar \
     --split val --mode testval
