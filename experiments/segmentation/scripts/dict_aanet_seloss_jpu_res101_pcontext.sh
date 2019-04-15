3e398f94796b3f12c45b377bb86ae7f748652b68
hlzhu <908865817@qq.com>
2019-04-15 14:23:58 +0400

    resnet

--- experiments/segmentation/scripts/dict_aanet_seloss_jpu_res101_pcontext.sh
+++ experiments/segmentation/scripts/dict_aanet_seloss_jpu_res101_pcontext.sh
@@ -9,4 +9,4 @@ python train.py --dataset pcontext \
 python test.py --dataset pcontext \
     --model dict_aanet --aux --jpu --se-loss --base-size 608 --crop-size 576 \
     --backbone resnet101 --resume runs/pcontext/dict_aanet/dict_aanet_seloss_jpu_res101_pcontext/model_best.pth.tar \
-    --split val --mode testval#!/usr/bin/env bash
+    --split val --mode testval
--- experiments/segmentation/scripts/dict_aanet_seloss_jpu_res50_pcontext.sh
+++ experiments/segmentation/scripts/dict_aanet_seloss_jpu_res50_pcontext.sh
@@ -10,3 +10,9 @@ python test.py --dataset pcontext \
     --model dict_aanet --aux --jpu --se-loss --base-size 608 --crop-size 576 \
     --backbone resnet50 --resume runs/pcontext/dict_aanet/dict_aanet_seloss_jpu_res50_pcontext/model_best.pth.tar \
     --split val --mode testval
+
+#test [multi-scale]
+python test.py --dataset pcontext \
+    --model dict_aanet --aux --jpu --se-loss --base-size 608 --crop-size 576 \
+    --backbone resnet50 --resume runs/pcontext/dict_aanet/dict_aanet_seloss_jpu_res50_pcontext/model_best.pth.tar \
+    --split val --mode testval --ms
