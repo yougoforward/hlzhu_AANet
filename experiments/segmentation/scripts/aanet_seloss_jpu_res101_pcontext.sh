5bb95c781940c0931fb23294a7c01148cd66e69e
hlzhu <908865817@qq.com>
2019-04-22 11:26:48 +0400

    resnet

--- experiments/segmentation/scripts/aanet_seloss_jpu_res101_pcontext.sh
+++ experiments/segmentation/scripts/aanet_seloss_jpu_res101_pcontext.sh
@@ -16,3 +16,10 @@ python test.py --dataset pcontext \
     --model aanet --aux --jpu --se-loss --base-size 608 --crop-size 576 \
     --backbone resnet101 --resume runs/pcontext/aanet/aanet_seloss_jpu_res101_pcontext/model_best.pth.tar \
     --split val --mode testval --ms
+
+
+
+#fps
+CUDA_VISIBLE_DEVICES=0 python test_fps_params.py --dataset pcontext \
+    --model aanet --jpu --aux --se-loss --base-size 608 --crop-size 576 \
+    --backbone resnet101
--- experiments/segmentation/test.py
+++ experiments/segmentation/test.py
@@ -58,7 +58,7 @@ def test(args):
         print("=> loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint['epoch']))
 
     print(model)
-    scales = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25] if args.dataset == 'citys' else \
+    scales = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25] if args.dataset == 'cityscapes' else \
         [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
     if not args.ms:
         scales = [1.0]
