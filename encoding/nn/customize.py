fd642fcdd7d2579ba8069e8fd98ed3dbe92a40bc
hlzhu <908865817@qq.com>
2019-04-24 19:16:25 +0400

    aanet jpu

--- encoding/nn/customize.py
+++ encoding/nn/customize.py
@@ -104,7 +104,7 @@ class metric_SegmentationLosses(CrossEntropyLoss):
             bs, N, N = metric_pred.size()
             # print(target.size())
             metric_target = F.interpolate(target.float().unsqueeze(dim=1), scale_factor=0.125, mode='nearest')
-            metric_target = torch.where(metric_target.view(bs,1,N,1).expand(bs,1,N,N)==metric_target.view(bs,1,1,N).expand(bs,1,N,N), torch.ones(bs,1,N,N), torch.zeros(bs,1,N,N))
+            metric_target = torch.where(metric_target.view(bs,1,N,1).expand(bs,1,N,N)==metric_target.view(bs,1,1,N).expand(bs,1,N,N), torch.ones(bs,1,N,N).cuda(), torch.zeros(bs,1,N,N).cuda())
             loss1 = super(metric_SegmentationLosses, self).forward(pred1, target)
             loss2 = super(metric_SegmentationLosses, self).forward(pred2, target)
             loss3 = self.bceloss(torch.sigmoid(se_pred), se_target)
