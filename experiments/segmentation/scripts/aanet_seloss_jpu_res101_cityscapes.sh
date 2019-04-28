370fa3906a67babb24afe2aba39d551c305e1176
hlzhu <908865817@qq.com>
2019-04-28 11:44:58 +0400

    aanet jpu

--- experiments/segmentation/scripts/aanet_seloss_jpu_res101_cityscapes.sh
+++ experiments/segmentation/scripts/aanet_seloss_jpu_res101_cityscapes.sh
@@ -2,17 +2,17 @@
 
 #train
 python train.py --dataset cityscapes \
-    --model aanet --aux --jpu --se-loss --batch-size 8 --base-size 1024 --crop-size 768 \
+    --model aanet --aux --jpu --se-loss --batch-size 16 --base-size 1024 --crop-size 768 \
     --backbone resnet101 --checkname aanet_seloss_jpu_res101_cityscapes
 
 #test [single-scale]
 python test.py --dataset cityscapes \
-    --model aanet --aux --jpu --se-loss --batch-size 8 --base-size 2048 --crop-size 768 \
+    --model aanet --aux --jpu --se-loss --batch-size 16 --base-size 2048 --crop-size 768 \
     --backbone resnet101 --resume runs/cityscapes/aanet/aanet_seloss_jpu_res101_cityscapes/model_best.pth.tar \
     --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset cityscapes \
-    --model aanet --aux --jpu --se-loss --batch-size 8 --base-size 2048 --crop-size 1024 \
+    --model aanet --aux --jpu --se-loss --batch-size 16 --base-size 2048 --crop-size 1024 \
     --backbone resnet101 --resume runs/cityscapes/aanet/aanet_seloss_jpu_res101_cityscapes/model_best.pth.tar \
     --split val --mode testval --ms
