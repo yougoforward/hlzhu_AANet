4d28e7b8a393198f2ba0eb9e4b17645505eeeeb9
hlzhu <908865817@qq.com>
2019-06-13 17:25:18 +0400

    amaca

--- experiments/segmentation/scripts/amgapaca_res101_pcontext.sh
+++ experiments/segmentation/scripts/amgapaca_res101_pcontext.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #train
 python train.py --dataset pcontext \
-    --model amgapaca --aux --dilated --base-size 608 --crop-size 576 \
-    --backbone resnet101 --checkname amgapaca_res101_pcontext
+    --model amca_gapaca --aux --dilated --base-size 608 --crop-size 576 \
+    --backbone resnet101 --checkname amca_gapaca_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model amgapaca --aux --dilated --base-size 608 --crop-size 576 \
-    --backbone resnet101 --resume runs/pcontext/amgapaca/amgapaca_res101_pcontext/model_best.pth.tar \
+    --model amca_gapaca --aux --dilated --base-size 608 --crop-size 576 \
+    --backbone resnet101 --resume runs/pcontext/amca_gapaca/amca_gapaca_res101_pcontext/model_best.pth.tar \
     --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
-    --model amgapaca --aux --dilated --base-size 608 --crop-size 576 \
-    --backbone resnet101 --resume runs/pcontext/amgapaca/amgapaca_res101_pcontext/model_best.pth.tar \
+    --model amca_gapaca --aux --dilated --base-size 608 --crop-size 576 \
+    --backbone resnet101 --resume runs/pcontext/amca_gapaca/amca_gapaca_res101_pcontext/model_best.pth.tar \
      --split val --mode testval --ms
