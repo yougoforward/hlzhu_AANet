118d918ac20d54fe430b2a76f435e1c9b8ab9081
hlzhu <908865817@qq.com>
2019-06-19 22:03:43 +0400

    amaca

--- experiments/segmentation/scripts/asp_pgf_res50_pcontext.sh
+++ experiments/segmentation/scripts/asp_pgf_res50_pcontext.sh
@@ -2,15 +2,15 @@
 
 #train
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --dataset pcontext \
-    --model asp_pgfnet --jpu --aux --se_loss \
+    --model asp_pgfnet --jpu --aux --se-loss \
     --backbone resnet50 --checkname asp_pgfnet_res50_pcontext
 
 #test [single-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model asp_pgfnet --jpu --aux --se_loss \
+    --model asp_pgfnet --jpu --aux --se-loss \
     --backbone resnet50 --resume runs/pcontext/asp_pgfnet/asp_pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model asp_pgfnet --jpu --aux --se_loss \
+    --model asp_pgfnet --jpu --aux --se-loss \
     --backbone resnet50 --resume runs/pcontext/asp_pgfnet/asp_pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/pgf_res50_pcontext.sh
+++ experiments/segmentation/scripts/pgf_res50_pcontext.sh
@@ -2,15 +2,15 @@
 
 #train
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se_loss \
+    --model pgfnet --jpu --aux --se-loss \
     --backbone resnet50 --checkname pgfnet_res50_pcontext
 
 #test [single-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se_loss \
+    --model pgfnet --jpu --aux --se-loss \
     --backbone resnet50 --resume runs/pcontext/pgfnet/pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se_loss \
+    --model pgfnet --jpu --aux --se-loss \
     --backbone resnet50 --resume runs/pcontext/pgfnet/pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
