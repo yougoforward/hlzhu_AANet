4a7495385feccd1a82c5df6cb6c10307c5ccdfd4
hlzhu <908865817@qq.com>
2019-06-20 09:58:30 +0400

    amaca

--- experiments/segmentation/scripts/asp_pgf_res50_pcontext.sh
+++ experiments/segmentation/scripts/asp_pgf_res50_pcontext.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 #train
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --dataset pcontext \
-    --model asp_pgfnet --jpu --aux --se-loss \
+python train.py --dataset pcontext \
+    --model asp_pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --checkname asp_pgfnet_res50_pcontext
 
 #test [single-scale]
-CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model asp_pgfnet --jpu --aux --se-loss \
+python test.py --dataset pcontext \
+    --model asp_pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume runs/pcontext/asp_pgfnet/asp_pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model asp_pgfnet --jpu --aux --se-loss \
+python test.py --dataset pcontext \
+    --model asp_pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume runs/pcontext/asp_pgfnet/asp_pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/pgf_res50_pcontext.sh
+++ experiments/segmentation/scripts/pgf_res50_pcontext.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 #train
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se-loss --batch-size 8\
+python train.py --dataset pcontext \
+    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --checkname pgfnet_res50_pcontext
 
 #test [single-scale]
-CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se-loss \
+python test.py --dataset pcontext \
+    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume runs/pcontext/pgfnet/pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-CUDA_VISIBLE_DEVICES=0,1,2,3 python test.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se-loss \
+python test.py --dataset pcontext \
+    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume runs/pcontext/pgfnet/pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
