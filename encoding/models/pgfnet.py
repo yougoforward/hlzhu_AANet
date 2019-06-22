f7e926c2bd5d2706aeacfae33af6452ff395cf8a
hlzhu <908865817@qq.com>
2019-06-22 15:02:22 +0400

    amaca

--- encoding/models/pgfnet.py
+++ encoding/models/pgfnet.py
@@ -125,12 +125,16 @@ class PyramidGuidedFusion(nn.Module):
         self.pool2 = nn.Sequential(nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=2, padding=1, bias=False),
                                 norm_layer(in_channels),
                                 nn.ReLU(True))
-        self.pool3 = self.pool2
-        self.pool4 = self.pool2
+        self.pool3 = nn.Sequential(nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=2, padding=1, bias=False),
+                                norm_layer(in_channels),
+                                nn.ReLU(True))
+        self.pool4 = nn.Sequential(nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=2, padding=1, bias=False),
+                                norm_layer(in_channels),
+                                nn.ReLU(True))
 
         self.gf2 = GuidedFusion(in_channels, in_channels//2, norm_layer)
-        self.gf3 = self.gf2
-        self.gf4 = self.gf2
+        self.gf3 = GuidedFusion(in_channels, in_channels//2, norm_layer)
+        self.gf4 = GuidedFusion(in_channels, in_channels//2, norm_layer)
 
         self.se_loss = se_loss
         if self.se_loss:
@@ -146,15 +150,15 @@ class PyramidGuidedFusion(nn.Module):
         d1 = x
         d2=self.pool2(d1)
         d3=self.pool3(d2)
-        # d4=self.pool4(d3)
-        d4=d3
+        d4=self.pool4(d3)
+        # d4=d3
         if self.se_loss:
             gap_feat = self.gap(d4)
             gamma = self.se(gap_feat)
             d4 = F.relu(d4 + d4 * gamma)
 
-        # u3 = self.gf4(d3, d4)
-        u3=d4
+        u3 = self.gf4(d3, d4)
+        # u3=d4
         u2 = self.gf3(d2, u3)
         u1 = self.gf2(d1, u2)
         outputs= [u1]
--- experiments/segmentation/scripts/pgf_res50_pcontext.sh
+++ experiments/segmentation/scripts/pgf_res50_pcontext.sh
@@ -2,15 +2,15 @@
 
 #train
 python train.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 512 \
+    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --checkname pgfnet_res50_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 512 \
+    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume runs/pcontext/pgfnet/pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
-    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 512 \
+    --model pgfnet --jpu --aux --se-loss --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume runs/pcontext/pgfnet/pgfnet_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
