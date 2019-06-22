7f6e29f7e885bc2a3c40901a6f7745920d1fe3ee
hlzhu <908865817@qq.com>
2019-06-22 22:41:12 +0400

    amaca

--- encoding/models/asp_pgfnet.py
+++ encoding/models/asp_pgfnet.py
@@ -66,27 +66,27 @@ class GuidedFusion(nn.Module):
     """
     exploit self-attentin for  adjacent scale fusion
     """
-    def __init__(self, in_channels, query_dim, norm_layer=nn.BatchNorm2d):
+    def __init__(self, in_channels, query_dim, norm_layer):
         super(GuidedFusion, self).__init__()
         self.key_channels = query_dim
-        self.query_conv = nn.Conv2d(in_channels=in_channels, out_channels=self.key_channels,
-                      kernel_size=1, stride=1, padding=0)
-        # self.query_conv =  nn.Sequential(
-        #     nn.Conv2d(in_channels=in_channels, out_channels=self.key_channels,
-        #               kernel_size=1, stride=1, padding=0),
-        #     norm_layer(self.key_channels),
-        #     nn.ReLU(True)
-        # )
-        self.fuse_conv =  nn.Sequential(
+        # self.query_conv = nn.Conv2d(in_channels=in_channels, out_channels=query_dim,
+        #               kernel_size=1, stride=1, padding=0)
+        self.query_conv =  nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=self.key_channels,
+                      kernel_size=1, stride=1, padding=0, bias=False),
+            norm_layer(self.key_channels),
+        )
+
+        self.value_conv =  nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=in_channels,
-                      kernel_size=1, stride=1, padding=0),
-            norm_layer(in_channels),
-            nn.ReLU(True)
+                      kernel_size=1, stride=1, padding=0, bias=False),
+            norm_layer(in_channels)
         )
         self.key_conv = self.query_conv
 
         self.gamma = nn.Parameter(torch.zeros(1))
         self.softmax = nn.Softmax(dim=-1)
+        self.relu=nn.ReLU(True)
 
     def forward(self, low_level, high_level):
 
@@ -98,17 +98,18 @@ class GuidedFusion(nn.Module):
 
         query = self.query_conv(low_level).view(m_batchsize, -1, hl * wl).permute(0, 2, 1) # m, hl*wl, c
         key = self.key_conv(high_level).view(m_batchsize, -1, hh * wh) # m, c, hh*wh
+
+        value = self.value_conv(high_level)
+
         energy = torch.bmm(query, key)        # C, hl*wl,hh*wh
 
         energy = (self.key_channels ** -.5) * energy
 
         attention = self.softmax(energy)
-        value = high_level
         out = torch.bmm(value.view(m_batchsize, C, hh*wh), attention.permute(0, 2, 1))
         out = out.view(m_batchsize, C, hl, wl)
 
-        out = self.gamma * out + low_level
-        out = self.fuse_conv(out)
+        out = self.relu(self.gamma * out + low_level)
         return out
 
 
