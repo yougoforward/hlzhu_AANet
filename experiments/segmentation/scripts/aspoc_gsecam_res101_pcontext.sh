cc3f8bd4027306971f7388424ded6716f48eab0e
hlzhu <908865817@qq.com>
2019-07-22 21:28:20 +0400

    amaca

--- experiments/segmentation/scripts/aspoc_gsecam_res101_pcontext.sh
+++ experiments/segmentation/scripts/aspoc_gsecam_res101_pcontext.sh
@@ -2,17 +2,17 @@
 
 #train
 python train.py --dataset pcontext \
-    --model aspoc_gsecam --aux --dilated --base-size 608 --crop-size 576 \
+    --model aspoc_gsecam --aux --dilated --base-size 520 --crop-size 480 \
     --backbone resnet101 --checkname aspoc_gsecam_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model aspoc_gsecam --aux --dilated --base-size 608 --crop-size 576 \
+    --model aspoc_gsecam --aux --dilated --base-size 520 --crop-size 480 \
     --backbone resnet101 --resume runs/pcontext/aspoc_gsecam/aspoc_gsecam_res101_pcontext/model_best.pth.tar \
     --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
-    --model aspoc_gsecam --aux --dilated --base-size 608 --crop-size 576 \
+    --model aspoc_gsecam --aux --dilated --base-size 520 --crop-size 480 \
     --backbone resnet101 --resume runs/pcontext/aspoc_gsecam/aspoc_gsecam_res101_pcontext/model_best.pth.tar \
      --split val --mode testval --ms
