9c6fdd51782b814de913dcbf5913783227a19bea
hlzhu <908865817@qq.com>
2019-08-04 09:42:24 +0400

    mstest

--- experiments/segmentation/scripts/amca2_res50_pcontext.sh
+++ experiments/segmentation/scripts/amca2_res50_pcontext.sh
@@ -12,4 +12,4 @@ python test.py --dataset pcontext \
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model amca2 --aux --dilated --base-size 608 --crop-size 576 \
-    --backbone resnet50 --resume runs/pcontext/amca2/amca2_res50_pcontext/model_best.pth.tar --split val --mode testval --ms#!/usr/bin/env bash
+    --backbone resnet50 --resume runs/pcontext/amca2/amca2_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
