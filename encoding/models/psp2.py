b574f4c3a1fe9d60fd8f0c0587aac28c27db0aaa
yougoforward <908865817@qq.com>
2019-09-11 17:54:00 +0800

    pap6

--- encoding/models/psp2.py
+++ encoding/models/psp2.py
@@ -16,7 +16,7 @@ from .fcn import FCNHead
 class PSP2(BaseNet):
     def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
         super(PSP2, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
-        self.head = PSPHead(2048, nclass, norm_layer, self._up_kwargs)
+        self.head = PSP2Head(2048, nclass, norm_layer, self._up_kwargs)
         if aux:
             self.auxlayer = FCNHead(1024, nclass, norm_layer)
 
