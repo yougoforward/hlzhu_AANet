4d0dbea888e995231073a57e3a6a8150d1e83d6b
yougoforward <908865817@qq.com>
2019-09-12 08:46:31 +0800

    pap6

--- experiments/segmentation/scripts/psp2_res50_pcontext.sh
+++ experiments/segmentation/scripts/psp2_res50_pcontext.sh
@@ -7,9 +7,9 @@ python train.py --dataset pcontext \
 #test [single-scale]
 python test.py --dataset pcontext \
     --model psp2 --aux --dilated --base-size 608 --crop-size 576 \
-    --backbone resnet50 --resume runs/pcontext/psp2/psp2_res50_pcontext/model_best.pth.tar --split val --mode testval
+    --backbone resnet50 --resume runs/pcontext/psp2/psp2_res50_pcontext/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model psp2 --aux --dilated --base-size 608 --crop-size 576 \
-    --backbone resnet50 --resume runs/pcontext/psp2/psp2_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
+    --backbone resnet50 --resume runs/pcontext/psp2/psp2_res50_pcontext/checkpoint.pth.tar --split val --mode testval --ms
