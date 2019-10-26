3dd18326ce5e073490bf0c874a78d641f22dec49
yougoforward <908865817@qq.com>
2019-10-26 14:29:31 +0800

    psp

--- encoding/models/psaa7.py
+++ encoding/models/psaa7.py
@@ -40,7 +40,7 @@ class psaa7NetHead(nn.Module):
         inter_channels = in_channels // 8
 
         self.aa_psaa7 = psaa7_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
-        self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(inter_channels, out_channels, 1))
+        self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
 
     def forward(self, x):
 
@@ -140,10 +140,12 @@ class psaa7_Module(nn.Module):
         # gcam
         gap =self.gap(x)
         # out = self.guided_cam(self.skip_conv(x), out)
-        out = self.reduce_conv(torch.cat([gap, out], dim=1))
+        # out = self.reduce_conv(torch.cat([gap, out], dim=1))
+        #
+        # # se
+        # out = out+self.se(out)*out
 
-        # se
-        out = out+self.se(out)*out
+        out = torch.cat([gap, out], dim=1)
         return out
 
 
