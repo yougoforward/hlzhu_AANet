fee90e76a75be924748399f21fbf7893c6552136
yougoforward <908865817@qq.com>
2019-11-04 09:14:14 +0800

    aug

--- encoding/models/psaa10.py
+++ encoding/models/psaa10.py
@@ -133,7 +133,7 @@ class psaa10_Module(nn.Module):
         # psaa
         y1 = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
         y = torch.stack((feat0, feat1, feat2, feat3, feat4), dim=-1)
-        out = self.psaa(y1, y)
+        out = self.psaa(x, y1, y)
 
 
         # # cat and project
@@ -271,14 +271,14 @@ class Psaa_Module(nn.Module):
     # Ref from SAGAN
     def __init__(self, out_channels, norm_layer):
         super(Psaa_Module, self).__init__()
-        self.project = nn.Sequential(nn.Conv2d(5 * out_channels, 5, 1, bias=True))
+        self.project = nn.Sequential(nn.Conv2d(2048+5 * out_channels, 5, 1, bias=True))
 
         self.fuse_conv = nn.Sequential(nn.Conv2d(out_channels, out_channels, 1, padding=0, bias=False),
                                        norm_layer(out_channels),
                                        nn.ReLU(True))
         self.gamma = nn.Parameter(torch.zeros(1))
 
-    def forward(self, cat, stack):
+    def forward(self, x, cat, stack):
         """
             inputs :
                 x : input feature maps( B X C X H X W)
@@ -288,7 +288,7 @@ class Psaa_Module(nn.Module):
         """
         n, c, h, w, s = stack.size()
 
-        energy = self.project(cat)
+        energy = self.project(torch.cat([x, cat], dim=1))
         attention = torch.softmax(energy, dim=1)
         yv = stack.view(n, c, h * w, 5).permute(0, 2, 1, 3)
         out = torch.matmul(yv, attention.view(n, 5, h * w).permute(0, 2, 1).unsqueeze(dim=3))
