2b18c926f68b9bd881d44c19d2dade2ad3d0984e
yougoforward <908865817@qq.com>
2019-11-04 17:35:29 +0800

    aug

--- encoding/models/psaa10.py
+++ encoding/models/psaa10.py
@@ -37,7 +37,7 @@ class psaa10NetHead(nn.Module):
                  atrous_rates=(12, 24, 36)):
         super(psaa10NetHead, self).__init__()
         self.se_loss = se_loss
-        inter_channels = in_channels // 4
+        inter_channels = in_channels // 8
 
         self.aa_psaa10 = psaa10_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
         self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(inter_channels, out_channels, 1))
@@ -271,7 +271,7 @@ class Psaa_Module(nn.Module):
     # Ref from SAGAN
     def __init__(self, out_channels, norm_layer):
         super(Psaa_Module, self).__init__()
-        self.project = nn.Sequential(nn.Conv2d(2048+5 * out_channels, 5, 1, bias=True))
+        self.project = nn.Sequential(nn.Conv2d(5 * out_channels, 5, 1, bias=True))
 
         self.fuse_conv = nn.Sequential(nn.Conv2d(out_channels, out_channels, 1, padding=0, bias=False),
                                        norm_layer(out_channels),
@@ -288,7 +288,7 @@ class Psaa_Module(nn.Module):
         """
         n, c, h, w, s = stack.size()
 
-        energy = self.project(torch.cat([x, cat], dim=1))
+        energy = self.project(cat)
         attention = torch.softmax(energy, dim=1)
         yv = stack.view(n, c, h * w, 5).permute(0, 2, 1, 3)
         out = torch.matmul(yv, attention.view(n, 5, h * w).permute(0, 2, 1).unsqueeze(dim=3))
--- encoding/models/psaa9.py
+++ encoding/models/psaa9.py
@@ -257,7 +257,7 @@ class Psaa_Module(nn.Module):
     # Ref from SAGAN
     def __init__(self, out_channels, norm_layer):
         super(Psaa_Module, self).__init__()
-        self.project = nn.Sequential(nn.Conv2d(2048+5 * out_channels, 5, 1, bias=True))
+        self.project = nn.Sequential(nn.Conv2d(5 * out_channels, 5, 1, bias=True))
 
         self.fuse_conv = nn.Sequential(nn.Conv2d(out_channels, out_channels, 1, padding=0, bias=False),
                                        norm_layer(out_channels),
@@ -274,7 +274,7 @@ class Psaa_Module(nn.Module):
         """
         n, c, h, w, s = stack.size()
 
-        energy = self.project(torch.cat([x, cat], dim=1))
+        energy = self.project(cat)
         attention = torch.softmax(energy, dim=1)
         yv = stack.view(n, c, h * w, 5).permute(0, 2, 1, 3)
         out = torch.matmul(yv, attention.view(n, 5, h * w).permute(0, 2, 1).unsqueeze(dim=3))
