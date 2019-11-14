5c701521c205b533aafe89adef6e2e4b9b02c7fa
yougoforward <908865817@qq.com>
2019-11-14 11:33:17 +0800

    psaaconv

--- encoding/models/psaa32.py
+++ encoding/models/psaa32.py
@@ -34,7 +34,7 @@ class psaa32Net(BaseNet):
 
 class psaa32NetHead(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, se_loss, jpu=False, up_kwargs=None,
-                 atrous_rates=(6, 12, 24, 36)):
+                 atrous_rates=(12, 24, 36)):
         super(psaa32NetHead, self).__init__()
         self.se_loss = se_loss
         inter_channels = in_channels // 4
@@ -80,12 +80,11 @@ class psaa32Pooling(nn.Module):
         # return pool.repeat(1,1,h,w)
         return pool.expand(bs, self.out_chs, h, w)
 
-
 class psaa32_Module(nn.Module):
     def __init__(self, in_channels, out_channels, atrous_rates, norm_layer, up_kwargs):
         super(psaa32_Module, self).__init__()
         # out_channels = in_channels // 8
-        rate1, rate2, rate3, rate4 = tuple(atrous_rates)
+        rate1, rate2, rate3 = tuple(atrous_rates)
         self.b0 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 1, bias=False),
             norm_layer(out_channels),
@@ -93,20 +92,16 @@ class psaa32_Module(nn.Module):
         self.b1 = psaa32Conv(in_channels, out_channels, rate1, norm_layer)
         self.b2 = psaa32Conv(in_channels, out_channels, rate2, norm_layer)
         self.b3 = psaa32Conv(in_channels, out_channels, rate3, norm_layer)
-        self.b4 = psaa32Conv(in_channels, out_channels, rate4, norm_layer)
+        self.b4 = psaa32Pooling(in_channels, out_channels, norm_layer, up_kwargs)
+    
 
-        self.b5 = psaa32Pooling(in_channels, out_channels, norm_layer, up_kwargs)
 
         self._up_kwargs = up_kwargs
-        # self.psaa_conv = nn.Sequential(
-        #     nn.Conv2d(in_channels + 6 * out_channels, out_channels, 1, padding=0, bias=False),
-        #     norm_layer(out_channels),
-        #     nn.ReLU(True),
-        #     nn.Conv2d(out_channels, 6, 1, bias=True))
-        self.project = nn.Sequential(nn.Conv2d(in_channels=6 * out_channels, out_channels=out_channels,
-                                               kernel_size=1, stride=1, padding=0, bias=False),
-                                     norm_layer(out_channels),
-                                     nn.ReLU(True))
+        self.psaa_conv = nn.Sequential(nn.Conv2d(in_channels+5*out_channels, 5, 1, bias=True))
+        self.project = nn.Sequential(nn.Conv2d(in_channels=5*out_channels, out_channels=out_channels,
+                      kernel_size=1, stride=1, padding=0, bias=False),
+                      norm_layer(out_channels),
+                      nn.ReLU(True))
 
     def forward(self, x):
         feat0 = self.b0(x)
@@ -114,22 +109,18 @@ class psaa32_Module(nn.Module):
         feat2 = self.b2(x)
         feat3 = self.b3(x)
         feat4 = self.b4(x)
-        feat5 = self.b5(x)
-
         n, c, h, w = feat0.size()
 
         # psaa
-        y1 = torch.cat((feat0, feat1, feat2, feat3, feat4, feat5), 1)
-        # y = torch.stack((feat0, feat1, feat2, feat3, feat4, feat5), dim=-1)
-        # psaa_feat = self.psaa_conv(torch.cat([x, y1], dim=1))
-        # psaa_att = torch.sigmoid(psaa_feat)
-        # psaa_att_list = torch.split(psaa_att, 1, dim=1)
-        #
-        # y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2,
-        #                 psaa_att_list[3] * feat3, psaa_att_list[4] * feat4, psaa_att_list[5]*feat5), 1)
-        out = self.project(y1)
-        return out
+        y1 = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
+        y = torch.stack((feat0, feat1, feat2, feat3, feat4), dim=-1)
+        psaa_feat = self.psaa_conv(torch.cat([x,y1], dim=1))
+        psaa_att = torch.sigmoid(psaa_feat)
+        psaa_att_list = torch.split(psaa_att, 1, dim=1)
 
+        y2 = torch.cat((psaa_att_list[0]*feat0, psaa_att_list[1]*feat1, psaa_att_list[2]*feat2, psaa_att_list[3]*feat3, psaa_att_list[4]*feat4), 1)
+        out = self.project(y2)
+        return out
 
 def get_psaa32net(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
