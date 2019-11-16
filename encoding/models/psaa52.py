c70f42fda198a360f415a470ed35408011587b0d
yougoforward <908865817@qq.com>
2019-11-16 12:07:29 +0800

    psaaconv

--- encoding/models/psaa52.py
+++ encoding/models/psaa52.py
@@ -110,7 +110,8 @@ class psaa52_Module(nn.Module):
         # self.key_conv4 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
         # self.scale_spatial_agg = ss_Module(out_channels, norm_layer)
 
-        self.pam = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
+        # self.pam = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
+        self.gap = psaa52Pooling(in_channels, out_channels, norm_layer, up_kwargs)
 
     def forward(self, x):
         feat0 = self.b0(x)
@@ -131,8 +132,8 @@ class psaa52_Module(nn.Module):
                         psaa_att_list[3] * feat3, psaa_att_list[4] * feat4), 1)
         out = self.project(y2)
 
-        out2 = self.pam(out)
-        out = torch.cat([out, out2], dim=1)
+        # out2 = self.pam(out)
+        # out = torch.cat([out, out2], dim=1)
 
         # #scale spatial guided attention aggregation
 
@@ -145,7 +146,9 @@ class psaa52_Module(nn.Module):
 
         # key_stack = torch.stack((key0, key1, key2, key3, key4), dim=-1)
         # out = self.scale_spatial_agg(query, out, key_stack, fea_stack)
-
+        #gp
+        gp = self.gap(x)
+        out = torch.cat([out, gp], dim=1)
         return out
 
 class ss_Module(nn.Module):
