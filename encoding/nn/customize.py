373ebd979f0e16458b8a6b477b4f00b48ecdc167
yougoforward <908865817@qq.com>
2019-11-20 12:44:35 +0800

    gp

--- encoding/nn/customize.py
+++ encoding/nn/customize.py
@@ -39,7 +39,7 @@ class SegmentationLovaszLosses(CrossEntropyLoss):
             return super(SegmentationLovaszLosses, self).forward(*inputs)
         elif not self.se_loss:
             pred1, pred2, target = tuple(inputs)
-            loss1 = super(SegmentationLosses, self).forward(pred1, target)
+            loss1 = super(SegmentationLovaszLosses, self).forward(pred1, target)
             # pred1 = F.softmax(input=pred1, dim=1)
             # loss1 = lovasz_softmax_flat(*flatten_probas(pred1, target, self.ignore_index),only_present=True)
             loss2 = super(SegmentationLovaszLosses, self).forward(pred2, target)
