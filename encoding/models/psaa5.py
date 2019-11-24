1da3e30a70b8c3e884bea2adbe01cdff5be489ea
yougoforward <908865817@qq.com>
2019-11-24 09:24:37 +0800

    ms

--- encoding/models/psaa5.py
+++ encoding/models/psaa5.py
@@ -8,14 +8,14 @@ from .mask_softmax import Mask_Softmax
 from .fcn import FCNHead
 from .base import BaseNet
 
-__all__ = ['psaa52Net', 'get_psaa52net']
+__all__ = ['psaa5Net', 'get_psaa5net']
 
 
-class psaa52Net(BaseNet):
+class psaa5Net(BaseNet):
     def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
-        super(psaa52Net, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
+        super(psaa5Net, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
 
-        self.head = psaa52NetHead(2048, nclass, norm_layer, se_loss, jpu=kwargs['jpu'], up_kwargs=self._up_kwargs)
+        self.head = psaa5NetHead(2048, nclass, norm_layer, se_loss, jpu=kwargs['jpu'], up_kwargs=self._up_kwargs)
         if aux:
             self.auxlayer = FCNHead(1024, nclass, norm_layer)
 
@@ -32,20 +32,20 @@ class psaa52Net(BaseNet):
         return tuple(x)
 
 
-class psaa52NetHead(nn.Module):
+class psaa5NetHead(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, se_loss, jpu=False, up_kwargs=None,
                  atrous_rates=(12, 24, 36, 48)):
-        super(psaa52NetHead, self).__init__()
+        super(psaa5NetHead, self).__init__()
         self.se_loss = se_loss
         inter_channels = in_channels // 4
 
-        self.aa_psaa52 = psaa52_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
+        self.aa_psaa5 = psaa5_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
         self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
         if self.se_loss:
             self.selayer = nn.Linear(inter_channels, out_channels)
 
     def forward(self, x):
-        feat_sum, gap_feat = self.aa_psaa52(x)
+        feat_sum, gap_feat = self.aa_psaa5(x)
         outputs = [self.conv8(feat_sum)]
         if self.se_loss:
             outputs.append(self.selayer(torch.squeeze(gap_feat)))
@@ -53,7 +53,7 @@ class psaa52NetHead(nn.Module):
         return tuple(outputs)
 
 
-def psaa52Conv(in_channels, out_channels, atrous_rate, norm_layer):
+def psaa5Conv(in_channels, out_channels, atrous_rate, norm_layer):
     block = nn.Sequential(
         nn.Conv2d(in_channels, 512, 1, padding=0,
                   dilation=1, bias=False),
@@ -66,9 +66,9 @@ def psaa52Conv(in_channels, out_channels, atrous_rate, norm_layer):
     return block
 
 
-class psaa52Pooling(nn.Module):
+class psaa5Pooling(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
-        super(psaa52Pooling, self).__init__()
+        super(psaa5Pooling, self).__init__()
         self._up_kwargs = up_kwargs
         self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
                                  nn.Conv2d(in_channels, out_channels, 1, bias=False),
@@ -86,20 +86,20 @@ class psaa52Pooling(nn.Module):
         return pool.expand(bs, self.out_chs, h, w)
 
 
-class psaa52_Module(nn.Module):
+class psaa5_Module(nn.Module):
     def __init__(self, in_channels, out_channels, atrous_rates, norm_layer, up_kwargs):
-        super(psaa52_Module, self).__init__()
+        super(psaa5_Module, self).__init__()
         # out_channels = in_channels // 4
         rate1, rate2, rate3, rate4 = tuple(atrous_rates)
         self.b0 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 1, bias=False),
             norm_layer(out_channels),
             nn.ReLU(True))
-        self.b1 = psaa52Conv(in_channels, out_channels, rate1, norm_layer)
-        self.b2 = psaa52Conv(in_channels, out_channels, rate2, norm_layer)
-        self.b3 = psaa52Conv(in_channels, out_channels, rate3, norm_layer)
-        self.b4 = psaa52Conv(in_channels, out_channels, rate4, norm_layer)
-        # self.b4 = psaa52Pooling(in_channels, out_channels, norm_layer, up_kwargs)
+        self.b1 = psaa5Conv(in_channels, out_channels, rate1, norm_layer)
+        self.b2 = psaa5Conv(in_channels, out_channels, rate2, norm_layer)
+        self.b3 = psaa5Conv(in_channels, out_channels, rate3, norm_layer)
+        self.b4 = psaa5Conv(in_channels, out_channels, rate4, norm_layer)
+        # self.b4 = psaa5Pooling(in_channels, out_channels, norm_layer, up_kwargs)
 
         self._up_kwargs = up_kwargs
         self.psaa_conv = nn.Sequential(nn.Conv2d(in_channels+5*out_channels, out_channels, 1, padding=0, bias=False),
@@ -111,16 +111,7 @@ class psaa52_Module(nn.Module):
                       norm_layer(out_channels),
                       nn.ReLU(True))
 
-        # self.query_conv = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
-        # self.key_conv0 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
-        # self.key_conv1 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
-        # self.key_conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
-        # self.key_conv3 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
-        # self.key_conv4 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels//8, kernel_size=1, padding=0)
-        # self.scale_spatial_agg = ss_Module(out_channels, norm_layer)
 
-        self.pam = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
-        # self.gap = psaa52Pooling(5*out_channels, out_channels, norm_layer, up_kwargs)
         self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
                             nn.Conv2d(in_channels, out_channels, 1, bias=False),
                             norm_layer(out_channels),
@@ -146,32 +137,18 @@ class psaa52_Module(nn.Module):
         y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2,
                         psaa_att_list[3] * feat3, psaa_att_list[4] * feat4), 1)
         out = self.project(y2)
-
-        # out2 = self.pam(out)
-        # out = torch.cat([out, out2], dim=1)
-
-        # #scale spatial guided attention aggregation
-
-        # query = self.query_conv(out) # n, c//4, h, w
-        # key0 = self.key_conv0(feat0) # n, c//4, h, w
-        # key1 = self.key_conv1(feat1)
-        # key2 = self.key_conv2(feat2)
-        # key3 = self.key_conv3(feat3)
-        # key4 = self.key_conv4(feat4)
-
-        # key_stack = torch.stack((key0, key1, key2, key3, key4), dim=-1)
-        # out = self.scale_spatial_agg(query, out, key_stack, fea_stack)
+        
         #gp
         gp = self.gap(x)
         se = self.se(gp)
         out = torch.cat([out+se*out, gp.expand(n, c, h, w)], dim=1)
         return out, gp
 
-def get_psaa52net(dataset='pascal_voc', backbone='resnet50', pretrained=False,
+def get_psaa5net(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
     # infer number of classes
     from ..datasets import datasets
-    model = psaa52Net(datasets[dataset.lower()].NUM_CLASS, backbone=backbone, root=root, **kwargs)
+    model = psaa5Net(datasets[dataset.lower()].NUM_CLASS, backbone=backbone, root=root, **kwargs)
     if pretrained:
         raise NotImplementedError
 
