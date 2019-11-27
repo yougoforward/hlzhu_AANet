9b9cca28eedf25df39092504dbc00c986fbe14ee
yougoforward <908865817@qq.com>
2019-11-27 19:23:26 +0800

    psp3

--- encoding/models/__init__.py
+++ encoding/models/__init__.py
@@ -162,8 +162,8 @@ def get_segmentation_model(name, **kwargs):
 
         'new_psp3': get_new_psp3,
         'new_psp4': get_new_psp4,
-        'new_psp5': get_new_psp5,
-        'new_psp6': get_new_psp6,
+        'new_psp5': get_new_psp5net,
+        'new_psp6': get_new_psp6net,
 
     }
     return models[name.lower()](**kwargs)
