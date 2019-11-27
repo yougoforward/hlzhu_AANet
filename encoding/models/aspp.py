05674fedf481ec8ba07548e0c9e1bb9a5800f7a5
yougoforward <908865817@qq.com>
2019-11-27 11:01:16 +0800

    psp3

--- encoding/models/aspp.py
+++ encoding/models/aspp.py
@@ -105,7 +105,7 @@ class ASPP_Module(nn.Module):
 
         self.project = nn.Sequential(
             nn.Conv2d(5 * out_channels, 2*out_channels, 1, bias=False),
-            norm_layer(out_channels),
+            norm_layer(2*out_channels),
             nn.ReLU(True))
 
     def forward(self, x):
