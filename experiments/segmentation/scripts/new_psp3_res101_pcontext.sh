9aaf44768de592f5b331aa438a38b5f09aa27a3d
yougoforward <908865817@qq.com>
2019-12-04 15:00:32 +0800

    psp6

--- experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
+++ experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
@@ -2,14 +2,14 @@
 #train
 python train.py --dataset pcontext \
     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname new_psp3_res101_pcontext
+    --backbone resnet101 --checkname new_psp3_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
