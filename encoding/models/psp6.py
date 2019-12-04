519fc764aed667c18f5f2162ff45f3fb31d8b0f0
yougoforward <908865817@qq.com>
2019-12-04 14:32:49 +0800

    psp6

--- encoding/models/psp6.py
+++ encoding/models/psp6.py
@@ -40,7 +40,7 @@ class psp6NetHead(nn.Module):
         inter_channels = in_channels // 4
 
         self.aa_psp6 = psp6_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
-        self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
+        self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2 * inter_channels, out_channels, 1))
         if self.se_loss:
             self.selayer = nn.Linear(inter_channels, out_channels)
 
@@ -99,42 +99,42 @@ class psp6_Module(nn.Module):
         self.b2 = psp6Conv(in_channels, out_channels, rate2, norm_layer)
         self.b3 = psp6Conv(in_channels, out_channels, rate3, norm_layer)
         self.b4 = nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 1, padding=0,
-                  dilation=1, bias=False),
-        norm_layer(out_channels),
-        nn.ReLU(True),
-        PAM_Module(in_dim=out_channels, key_dim=64,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer))
+            nn.Conv2d(in_channels, out_channels, 1, padding=0,
+                      dilation=1, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True),
+            PAM_Module(in_dim=out_channels, key_dim=64, value_dim=out_channels, out_dim=out_channels,
+                       norm_layer=norm_layer))
         # self.b4 = psp6Conv(in_channels, out_channels, rate4, norm_layer)
         # self.b4 = psp6Pooling(in_channels, out_channels, norm_layer, up_kwargs)
 
         self._up_kwargs = up_kwargs
-        self.psaa_conv = nn.Sequential(nn.Conv2d(in_channels+4*out_channels, out_channels, 1, padding=0, bias=False),
-                                    norm_layer(out_channels),
-                                    nn.ReLU(True),
-                                    nn.Conv2d(out_channels, 4, 1, bias=True))  
-        # self.psaa_conv = nn.Sequential(nn.Conv2d(in_channels+4*out_channels, 4, 1, padding=0, bias=True))       
-        self.project = nn.Sequential(nn.Conv2d(in_channels=4*out_channels, out_channels=out_channels,
-                      kernel_size=1, stride=1, padding=0, bias=False),
-                      norm_layer(out_channels),
-                      nn.ReLU(True))
+        self.psaa_conv = nn.Sequential(
+            nn.Conv2d(in_channels + 5 * out_channels, out_channels, 1, padding=0, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True),
+            nn.Conv2d(out_channels, 5, 1, bias=True))
+        # self.psaa_conv = nn.Sequential(nn.Conv2d(in_channels+4*out_channels, 4, 1, padding=0, bias=True))
 
+        self.project = nn.Sequential(nn.Conv2d(in_channels=5 * out_channels, out_channels=out_channels,
+                                               kernel_size=1, stride=1, padding=0, bias=False),
+                                     norm_layer(out_channels),
+                                     nn.ReLU(True))
 
         self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-                            nn.Conv2d(in_channels, out_channels, 1, bias=False),
-                            norm_layer(out_channels),
-                            nn.ReLU(True))
+                                 nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                                 norm_layer(out_channels),
+                                 nn.ReLU(True))
         self.se = nn.Sequential(
-                            nn.Conv2d(out_channels, out_channels, 1, bias=True),
-                            nn.Sigmoid())
+            nn.Conv2d(out_channels, out_channels, 1, bias=True),
+            nn.Sigmoid())
 
-        self.fuse = nn.Sequential(nn.Conv2d(in_channels=2*out_channels, out_channels=out_channels,
-                      kernel_size=1, stride=1, padding=0, bias=False),
-                      norm_layer(out_channels),
-                      nn.ReLU(True))
-        # self.pam0 = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
+        # self.pam0 = PAM_Module(in_dim=out_channels, key_dim=out_channels // 8, value_dim=out_channels,
+        #                        out_dim=out_channels, norm_layer=norm_layer)
         # self.pam1 = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
         # self.pam2 = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
         # self.pam3 = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
+
     def forward(self, x):
         feat0 = self.b0(x)
         feat1 = self.b1(x)
@@ -157,20 +157,18 @@ class psp6_Module(nn.Module):
         psaa_att_list = torch.split(psaa_att, 1, dim=1)
 
         y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2,
-                        psaa_att_list[3] * feat3), 1)
+                        psaa_att_list[3] * feat3, psaa_att_list[4] * feat4), 1)
         out = self.project(y2)
-        
-        #gp
+
+        # gp
         gp = self.gap(x)
         se = self.se(gp)
-        # out = torch.cat([self.pam0(out)+se*out, gp.expand(n, c, h, w)], dim=1)
-        out = torch.cat([feat4,self.fuse(torch.cat([out+se*out, gp.expand(n, c, h, w)], dim=1))], dim=1)
-
-        # out = feat4+self.fuse(out+out*se)+gp
+        out = torch.cat([out + se * out, gp.expand(n, c, h, w)], dim=1)
         return out, gp
 
+
 def get_psp6net(dataset='pascal_voc', backbone='resnet50', pretrained=False,
-                 root='~/.encoding/models', **kwargs):
+                    root='~/.encoding/models', **kwargs):
     # infer number of classes
     from ..datasets import datasets
     model = psp6Net(datasets[dataset.lower()].NUM_CLASS, backbone=backbone, root=root, **kwargs)
@@ -182,7 +180,8 @@ def get_psp6net(dataset='pascal_voc', backbone='resnet50', pretrained=False,
 
 class PAM_Module(nn.Module):
     """ Position attention module"""
-    #Ref from SAGAN
+
+    # Ref from SAGAN
     def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer):
         super(PAM_Module, self).__init__()
         self.chanel_in = in_dim
@@ -191,12 +190,14 @@ class PAM_Module(nn.Module):
         self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
         self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
         # self.value_conv = nn.Conv2d(in_channels=value_dim, out_channels=value_dim, kernel_size=1)
-        self.gamma = nn.Parameter(torch.zeros(1))
+        # self.gamma = nn.Parameter(torch.zeros(1))
+        self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True),
+                                   nn.Sigmoid())
 
         self.softmax = nn.Softmax(dim=-1)
-        self.fuse_conv = nn.Sequential(nn.Conv2d(value_dim, out_dim, 1, bias=False),
-                                       norm_layer(out_dim),
-                                       nn.ReLU(True))
+        # self.fuse_conv = nn.Sequential(nn.Conv2d(value_dim, out_dim, 1, bias=False),
+        #                                norm_layer(out_dim),
+        #                                nn.ReLU(True))
 
     def forward(self, x):
         """
@@ -209,17 +210,18 @@ class PAM_Module(nn.Module):
         xp = self.pool(x)
         m_batchsize, C, height, width = x.size()
         m_batchsize, C, hp, wp = xp.size()
-        proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
-        proj_key = self.key_conv(xp).view(m_batchsize, -1, wp*hp)
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)
+        proj_key = self.key_conv(xp).view(m_batchsize, -1, wp * hp)
         energy = torch.bmm(proj_query, proj_key)
         attention = self.softmax(energy)
         # proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
-        proj_value = xp.view(m_batchsize, -1, wp*hp)
-        
+        proj_value = xp.view(m_batchsize, -1, wp * hp)
+
         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
         out = out.view(m_batchsize, C, height, width)
         # out = F.interpolate(out, (height, width), mode="bilinear", align_corners=True)
 
-        out = self.gamma*out + x
+        gamma = self.gamma(x)
+        out = (1 - gamma) * out + gamma * x
         # out = self.fuse_conv(out)
         return out
---
+++ experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#train
+python train.py --dataset pcontext \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet50 --checkname new_psp3_res101_pcontext
+
+#test [single-scale]
+python test.py --dataset pcontext \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python test.py --dataset pcontext \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
