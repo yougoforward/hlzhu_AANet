0e2dd94c546f43926c933f824d379a78a2f2c3c3
yougoforward <908865817@qq.com>
2019-12-05 15:25:02 +0800

    psp6

--- encoding/models/psp4.py
+++ encoding/models/psp4.py
@@ -64,7 +64,7 @@ class OCR_Module(nn.Module):
 
         self.value_conv = nn.Sequential(nn.Conv1d(in_channels=in_channels, out_channels=self.key_dim, kernel_size=1),
                                         norm_layer(self.key_dim), nn.ReLU(True))
-        self.weights = nn.Sequential(nn.Conv1d(in_channels=self.key_dim, out_channels=out_channels, kernel_size=1),
+        self.weights = nn.Sequential(nn.Conv2d(in_channels=self.key_dim, out_channels=out_channels, kernel_size=1),
                                         norm_layer(out_channels), nn.ReLU(True))
 
         self.project = nn.Sequential(nn.Conv1d(in_channels=2*out_channels, out_channels=out_channels, kernel_size=1),
