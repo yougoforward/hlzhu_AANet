5314fe9d0df96fa40b696082c6bb59f9c9be6a4a
yougoforward <908865817@qq.com>
2019-12-13 09:51:42 +0800

    psp4

--- experiments/segmentation/scripts/new_psp6_lovasz_res101_pcontext.sh
+++ experiments/segmentation/scripts/new_psp6_lovasz_res101_pcontext.sh
@@ -2,14 +2,14 @@
 #train
 python train_lovasz.py --dataset pcontext \
     --model new_psp6 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname new_psp6_res101_pcontext
+    --backbone resnet101 --checkname new_psp6_lovasz_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model new_psp6 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/new_psp6/new_psp6_res101_pcontext/model_best.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/pcontext/new_psp6/new_psp6_lovasz_res101_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model new_psp6 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/new_psp6/new_psp6_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/pcontext/new_psp6/new_psp6_lovasz_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
