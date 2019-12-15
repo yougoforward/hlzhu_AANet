a3b8d00f251697f890b73ca867e1a85a130b3e72
yougoforward <908865817@qq.com>
2019-12-15 21:36:32 +0800

    ade20k

---
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#train
+python train.py --dataset cityscapes \
+    --model new_psp3 --aux --dilated --base-size 2048 --crop-size 769 \
+    --backbone resnet101 --checkname new_psp3_res101_cityscapes
+
+#test [single-scale]
+python test.py --dataset cityscapes \
+    --model new_psp3 --aux --dilated --base-size 2048 --crop-size 769 \
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python test.py --dataset cityscapes \
+    --model new_psp3 --aux --dilated --base-size 2048 --crop-size 769 \
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/model_best.pth.tar --split val --mode testval --ms
