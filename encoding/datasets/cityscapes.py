aeb04e384074ed5c716741085c707dc30d3b9c7a
yougoforward <908865817@qq.com>
2019-12-28 13:31:04 +0800

    city

--- encoding/datasets/cityscapes.py
+++ encoding/datasets/cityscapes.py
@@ -68,7 +68,7 @@ class CitySegmentation(BaseDataset):
         img = Image.open(self.images[index]).convert('RGB')
         if self.mode == 'test':
             if self.transform is not None:
-                img = np.array(img)/255
+                img = np.array(img, dtype=np.double)/255
                 img = self.transform(img)
             return img, os.path.basename(self.images[index])
         #mask = self.masks[index]
@@ -83,7 +83,7 @@ class CitySegmentation(BaseDataset):
             mask = self._mask_transform(mask)
         # general resize, normalize and toTensor
         if self.transform is not None:
-            img = np.array(img)/255
+            img = np.array(img, , dtype=np.double)/255
             img = self.transform(img)
         if self.target_transform is not None:
             mask = self.target_transform(mask)
