a0da198086304b7275b82f178a40eddce618a079
yougoforward <908865817@qq.com>
2020-01-06 21:12:51 +0800

    citytest

--- experiments/segmentation/scripts/new_psp3_res101_ade20k.sh
+++ experiments/segmentation/scripts/new_psp3_res101_ade20k.sh
@@ -2,14 +2,14 @@
 #train
 python train.py --dataset ade20k \
     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname new_psp3_res101_ade20k --no-val
+    --backbone resnet101 --checkname new_psp3_res101_ade20k
 
 #test [single-scale]
 python test.py --dataset ade20k \
     --model new_psp3 --aux --dilated --base-size 576 --crop-size 576 \
-    --backbone resnet101 --resume runs/ade20k/new_psp3/new_psp3_res101_ade20k/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/ade20k/new_psp3/new_psp3_res101_ade20k/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset ade20k \
     --model new_psp3 --aux --dilated --base-size 576 --crop-size 576 \
-    --backbone resnet101 --resume runs/ade20k/new_psp3/new_psp3_res101_ade20k/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/ade20k/new_psp3/new_psp3_res101_ade20k/model_best.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/new_psp3_res101_cityscapes.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes.sh
@@ -11,9 +11,9 @@
 #test [single-scale]
 python test.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --base-size 2048 --crop-size 768 \
-    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --base-size 2048 --crop-size 1024 \
-    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/model_best.pth.tar --split val --mode testval --ms
