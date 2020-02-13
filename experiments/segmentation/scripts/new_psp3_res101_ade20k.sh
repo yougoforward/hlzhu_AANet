1001656a8c246f5e15094a6101cd9cd6d37da439
yougoforward <908865817@qq.com>
2020-02-13 08:13:06 +0800

    no grid

--- experiments/segmentation/scripts/new_psp3_res101_ade20k.sh
+++ experiments/segmentation/scripts/new_psp3_res101_ade20k.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #train
 python train.py --dataset ade20k \
-    --model new_psp3 --aux --dilated --base-size 576 --crop-size 576 \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet101 --checkname new_psp3_res101_ade20k
 
 #test [single-scale]
 python test.py --dataset ade20k \
-    --model new_psp3 --aux --dilated --base-size 576 --crop-size 576 \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet101 --resume runs/ade20k/new_psp3/new_psp3_res101_ade20k/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset ade20k \
-    --model new_psp3 --aux --dilated --base-size 576 --crop-size 576 \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet101 --resume runs/ade20k/new_psp3/new_psp3_res101_ade20k/model_best.pth.tar --split val --mode testval --ms
