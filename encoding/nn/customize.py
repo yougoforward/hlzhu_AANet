d112fd0ef60f05f6a2070ea7b6d5a2b62c02d320
yougoforward <908865817@qq.com>
2020-02-29 23:09:17 +0800

    city

--- encoding/nn/customize.py
+++ encoding/nn/customize.py
@@ -206,7 +206,6 @@ class City_SegmentationLosses(CrossEntropyLoss):
         self.aux_weight = aux_weight
         self.bceloss = BCELoss(weight, size_average)
         self.ignore_index = ignore_index
-        weight = torch.FloatTensor([0.8373, 0.918, 0.866, 1.0345, 1.0166, 0.9969, 0.9754, 1.0489, 0.8786, 1.0023, 0.9539, 0.9843, 1.1116, 0.9037, 1.0865, 1.0955, 1.0865, 1.1529, 1.0507])
         self.criterion = torch.nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index)
         self.criterion_ohem = OhemCrossEntropy2d(ignore_label=ignore_index, thresh=0.7, min_kept=100000, use_weight=True)
 
