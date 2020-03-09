9fdf22251cf279b3fc6a306261ca6905a4b39d70
yougoforward <908865817@qq.com>
2020-03-09 22:36:55 +0800

    city finetune

--- experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
@@ -16,11 +16,11 @@ python train.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.01 --batch-size 8 --epochs 240 \
     --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --ft \
     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
-# finetune
-python train_city.py --dataset cityscapes \
-    --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.0001 --epochs 240 --lr-scheduler step \
-    --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --ft \
-    --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval_finetune --no-val --train-split trainval
+# # finetune
+# python train_city.py --dataset cityscapes \
+#     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.0001 --epochs 240 --lr-scheduler step \
+#     --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --ft \
+#     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval_finetune --no-val --train-split trainval
 #test [single-scale]
 # python test.py --dataset cityscapes \
 #     --model new_psp3 --aux --dilated --base-size 2048 --crop-size 768 \
