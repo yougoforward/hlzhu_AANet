b8b63b4cb79ebd1928cad787f2589123af54cda9
yougoforward <908865817@qq.com>
2020-03-17 23:14:20 +0800

    citytest

--- experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
@@ -30,12 +30,12 @@ python train.py --dataset cityscapes \
 #test [multi-scale]
 python test.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
-    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --split test --mode test --ms
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --split test --mode test --ms --save-folder cityscapes_best
 
 #test [multi-scale]
 python test.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
-    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --split test --mode test --ms --save-folder cityscapes_test
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --split test --mode test --ms --save-folder cityscapes_last
 # #test [multi-scale]
 # python test_whole_gpu.py --dataset cityscapes \
 #     --model new_psp3 --aux --dilated --base-size 2048 --crop-size 1024 \
