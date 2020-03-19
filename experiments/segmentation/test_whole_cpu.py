28be9bcc1f7f8099baf59034986efa1b4c61767d
yougoforward <908865817@qq.com>
2020-03-19 22:41:39 +0800

    city_whole_test

--- experiments/segmentation/test_whole_cpu.py
+++ experiments/segmentation/test_whole_cpu.py
@@ -106,7 +106,7 @@ def test(args):
         else:
             with torch.no_grad():
                 outputs = evaluator.forward(image[0].unsqueeze(0))
-                predicts = [testset.make_pred(torch.max(output, 1)[1].cpu().numpy())
+                predicts = [testset.make_pred(torch.max(output, 1)[1].numpy())
                             for output in outputs]
             for predict, impath in zip(predicts, dst):
                 mask = Image.fromarray(predict.squeeze().astype('uint8'))
