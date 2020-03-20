3e583f4154a4d141f47d6fa1888e489af9327a23
yougoforward <908865817@qq.com>
2020-03-20 22:44:20 +0800

    citywh

--- experiments/segmentation/scripts/new_psp3_res101_cityscapes_whole.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes_whole.sh
@@ -89,3 +89,8 @@
 python test_whole_gpu.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
     --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_finetune/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python test.py --dataset cityscapes \
+    --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
+    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_finetune/model_best.pth.tar --split val --mode testval
