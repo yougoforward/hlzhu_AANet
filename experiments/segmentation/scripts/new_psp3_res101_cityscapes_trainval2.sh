965aa9f7b1c04fdb62b33ff8fe4084f51550a193
yougoforward <908865817@qq.com>
2020-03-23 17:03:57 +0800

    city

--- experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval2.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval2.sh
@@ -8,14 +8,14 @@
 #     --model new_psp3 --aux --dilated --base-size 1024 --crop-size 768 --lr 0.01 --batch-size 8 --epochs 240 \
 #     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
 
-python train.py --dataset cityscapes \
-    --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.001 --batch-size 8 --epochs 5 \
-    --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval2 --train-split trainval
+# python train.py --dataset cityscapes \
+#     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.001 --batch-size 8 --epochs 5 \
+#     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval2 --train-split trainval
 
-python train.py --dataset cityscapes \
-    --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.01 --batch-size 8 --epochs 240 \
-    --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval2/checkpoint.pth.tar --ft \
-    --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval2 --train-split trainval
+# python train.py --dataset cityscapes \
+#     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.01 --batch-size 8 --epochs 240 \
+#     --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval2/checkpoint.pth.tar --ft \
+#     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval2 --train-split trainval
 
 python train.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.0001 --lr-scheduler step --batch-size 8 --epochs 20 \
