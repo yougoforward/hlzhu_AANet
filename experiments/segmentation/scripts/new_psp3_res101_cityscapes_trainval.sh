c3247c5c257f87abe15760ff4f2a9386a3c676f8
yougoforward <908865817@qq.com>
2020-03-27 11:32:49 +0800

    city_lovasz

--- experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
@@ -18,7 +18,7 @@
 #     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
 python train_lovasz.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.001 --batch-size 8 --epochs 240 \
-    --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --ft \
+    --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --ft \
     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
 # python train.py --dataset cityscapes \
 #     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.0001 --lr-scheduler step --batch-size 8 --epochs 20 \
@@ -41,10 +41,10 @@ python test.py --dataset cityscapes \
     --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
     --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --split test --mode test --ms --save-folder cityscapes_best
 
-#test [multi-scale]
-python test.py --dataset cityscapes \
-    --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
-    --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval_finetune/model_best.pth.tar --split test --mode test --ms --save-folder cityscapes_ft_best
+# #test [multi-scale]
+# python test.py --dataset cityscapes \
+#     --model new_psp3 --aux --dilated --multi-grid --base-size 2048 --crop-size 1024 \
+#     --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval_finetune/model_best.pth.tar --split test --mode test --ms --save-folder cityscapes_ft_best
 # #test [multi-scale]
 # python test_whole_gpu.py --dataset cityscapes \
 #     --model new_psp3 --aux --dilated --base-size 2048 --crop-size 1024 \
