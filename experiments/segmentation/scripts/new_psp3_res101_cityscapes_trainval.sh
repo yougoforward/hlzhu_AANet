d339a39afd8c4e361834b4f2b5e8515ccde928f3
yougoforward <908865817@qq.com>
2020-03-31 21:34:09 +0800

    ft

--- experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
+++ experiments/segmentation/scripts/new_psp3_res101_cityscapes_trainval.sh
@@ -16,10 +16,10 @@
 #     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.01 --batch-size 8 --epochs 240 \
 #     --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --ft \
 #     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
-python train_lovasz.py --dataset cityscapes \
-    --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.001 --batch-size 8 --epochs 240 \
-    --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --ft \
-    --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
+# python train_lovasz.py --dataset cityscapes \
+#     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.001 --batch-size 8 --epochs 240 \
+#     --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --ft \
+#     --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
 # python train.py --dataset cityscapes \
 #     --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.0001 --lr-scheduler step --batch-size 8 --epochs 20 \
 #     --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/checkpoint.pth.tar --ft \
@@ -35,6 +35,10 @@ python train_lovasz.py --dataset cityscapes \
 #     --model new_psp3 --aux --dilated --base-size 2048 --crop-size 768 \
 #     --backbone resnet101 --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes/checkpoint.pth.tar --split val --mode testval
 
+python train_lovasz.py --dataset cityscapes \
+    --model new_psp3 --aux --dilated --multi-grid --base-size 1024 --crop-size 768 --lr 0.0001 --batch-size 8 --epochs 20 \
+    --resume runs/cityscapes/new_psp3/new_psp3_res101_cityscapes_trainval/model_best.pth.tar --ft \
+    --backbone resnet101 --checkname new_psp3_res101_cityscapes_trainval --train-split trainval
 
 #test [multi-scale]
 python test.py --dataset cityscapes \
