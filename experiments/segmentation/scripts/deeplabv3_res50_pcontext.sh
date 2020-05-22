fade605356f5c0b10e69d815f87c56a343f5dbdf
yougoforward <908865817@qq.com>
2020-05-22 08:09:43 +0800

    v3pam512

--- experiments/segmentation/scripts/deeplabv3_res50_pcontext.sh
+++ experiments/segmentation/scripts/deeplabv3_res50_pcontext.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# #train
-# python train.py --dataset pcontext \
-#     --model deeplabv3 --aux --dilated --base-size 520 --crop-size 520 \
-#     --backbone resnet50 --checkname deeplabv3_res50_pcontext
+#train
+python train.py --dataset pcontext \
+    --model deeplabv3 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet50 --checkname deeplabv3_res50_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
@@ -11,7 +11,7 @@ python test.py --dataset pcontext \
     --split val --mode testval
 
 # #test [multi-scale]
-# python test.py --dataset pcontext \
-#     --model deeplabv3 --aux --dilated --base-size 520 --crop-size 520 \
-#     --backbone resnet50 --resume runs/pcontext/deeplabv3/deeplabv3_res50_pcontext/model_best.pth.tar \
-#      --split val --mode testval --ms
+python test.py --dataset pcontext \
+    --model deeplabv3 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume runs/pcontext/deeplabv3/deeplabv3_res50_pcontext/model_best.pth.tar \
+     --split val --mode testval --ms
