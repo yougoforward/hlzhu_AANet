875726f0cbe27719804103554af039c42104466d
yougoforward <908865817@qq.com>
2020-05-24 16:17:49 +0800

    gs78

--- encoding/models/gsnet9.py
+++ encoding/models/gsnet9.py
@@ -109,10 +109,10 @@ class gs_Module(nn.Module):
         self.psaa_conv = nn.Sequential(nn.Conv2d(5*out_channels, out_channels, 1, padding=0, bias=False),
                                     norm_layer(out_channels),
                                     nn.ReLU(True),
-                                    nn.Conv2d(out_channels, 4, 1, bias=True),
+                                    nn.Conv2d(out_channels, 5, 1, bias=True),
                                     nn.Sigmoid())  
 
-        self.project = nn.Sequential(nn.Conv2d(in_channels=4*out_channels, out_channels=out_channels,
+        self.project = nn.Sequential(nn.Conv2d(in_channels=5*out_channels, out_channels=out_channels,
                       kernel_size=1, stride=1, padding=0, bias=False),
                       norm_layer(out_channels),
                       nn.ReLU(True))
@@ -141,7 +141,7 @@ class gs_Module(nn.Module):
         psaa_att = self.psaa_conv(y1)
         psaa_att_list = torch.split(psaa_att, 1, dim=1)
 
-        y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] *feat3), 1)
+        y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] *feat3, psaa_att_list[4]*gp.expand(n, c, h, w)), 1)
 
         out = self.project(y2)
 
