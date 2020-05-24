4e5bd055431df0f61ca097f52dca39672daad275
yougoforward <908865817@qq.com>
2020-05-25 06:43:04 +0800

    gs

--- experiments/segmentation/scripts/gsnet_res101_pcontext.sh
+++ experiments/segmentation/scripts/gsnet_res101_pcontext.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
 python train.py --dataset pcontext \
-    --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname gsnet5_res50_pcontext
+    --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --checkname gsnet_res50_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/gsnet5/gsnet5_res50_pcontext/checkpoint.pth.tar --split val --mode testval
+    --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume runs/pcontext/gsnet/gsnet_res50_pcontext/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
-    --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/gsnet5/gsnet5_res50_pcontext/checkpoint.pth.tar --split val --mode testval --ms
+    --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume runs/pcontext/gsnet/gsnet_res50_pcontext/checkpoint.pth.tar --split val --mode testval --ms
