317a01f7edf1e819339d1873b07c1d1be2e3e4e0
yougoforward <908865817@qq.com>
2020-05-25 08:15:15 +0800

    gsnet

--- experiments/segmentation/scripts/gsnet_noatt_res101_pcontext.sh
+++ experiments/segmentation/scripts/gsnet_noatt_res101_pcontext.sh
@@ -2,14 +2,14 @@
 # train
 python train.py --dataset pcontext \
     --model gsnet_noatt --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname gsnet_noatt_res50_pcontext
+    --backbone resnet101 --checkname gsnet_noatt_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model gsnet_noatt --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/gsnet_noatt/gsnet_noatt_res50_pcontext/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/pcontext/gsnet_noatt/gsnet_noatt_res101_pcontext/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model gsnet_noatt --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/gsnet_noatt/gsnet_noatt_res50_pcontext/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/pcontext/gsnet_noatt/gsnet_noatt_res101_pcontext/checkpoint.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/gsnet_res101_pcontext.sh
+++ experiments/segmentation/scripts/gsnet_res101_pcontext.sh
@@ -2,14 +2,14 @@
 # train
 python train.py --dataset pcontext \
     --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname gsnet_res50_pcontext
+    --backbone resnet101 --checkname gsnet_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/gsnet/gsnet_res50_pcontext/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/pcontext/gsnet/gsnet_res101_pcontext/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model gsnet --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume runs/pcontext/gsnet/gsnet_res50_pcontext/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/pcontext/gsnet/gsnet_res101_pcontext/checkpoint.pth.tar --split val --mode testval --ms
