9647b1a046238887951fe321b9de1ccdb9da5446
yougoforward <908865817@qq.com>
2020-05-26 09:14:47 +0800

    gs

--- experiments/segmentation/scripts/gsnet5_res101_pcontext.sh
+++ experiments/segmentation/scripts/gsnet5_res101_pcontext.sh
@@ -2,14 +2,14 @@
 # train
 python train.py --dataset pcontext \
     --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname gsnet5_res50_pcontext
+    --backbone resnet101 --checkname gsnet5_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/gsnet5/gsnet5_res50_pcontext/model_best.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/pcontext/gsnet5/gsnet5_res101_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/gsnet5/gsnet5_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/pcontext/gsnet5/gsnet5_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
