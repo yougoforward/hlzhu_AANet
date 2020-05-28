871dd074ee207e1391616b8b7ae851d3c3e739f3
yougoforward <908865817@qq.com>
2020-05-28 20:55:27 +0800

    gs5

--- encoding/models/gsnet.py
+++ encoding/models/gsnet.py
@@ -124,11 +124,6 @@ class gs_Module(nn.Module):
         self.se = nn.Sequential(
                             nn.Conv2d(out_channels, out_channels, 1, bias=True),
                             nn.Sigmoid())
-
-        self.sec = nn.Sequential(
-                            nn.Conv2d(out_channels, out_channels, 1, bias=False),
-                            norm_layer(out_channels),
-                            nn.ReLU(True))
         self.pam0 = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
 
     def forward(self, x):
@@ -176,8 +171,8 @@ class PAM_Module(nn.Module):
         self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
         self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
         # self.value_conv = nn.Conv2d(in_channels=value_dim, out_channels=value_dim, kernel_size=1)
-        # self.gamma = nn.Parameter(torch.zeros(1))
-        self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
+        self.gamma = nn.Parameter(torch.zeros(1))
+        # self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
 
         self.softmax = nn.Softmax(dim=-1)
         # self.fuse_conv = nn.Sequential(nn.Conv2d(value_dim, out_dim, 1, bias=False),
@@ -206,7 +201,7 @@ class PAM_Module(nn.Module):
         out = out.view(m_batchsize, C, height, width)
         # out = F.interpolate(out, (height, width), mode="bilinear", align_corners=True)
 
-        gamma = self.gamma(x)
-        out = (1-gamma)*out + gamma*x
+        # gamma = self.gamma(x)
+        # out = (1-gamma)*out + gamma*x
         # out = self.fuse_conv(out)
-        return out
+        return x+self.gamma*out
