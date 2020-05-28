f96b20b937d41e1b9567b027f7a2b5dd46415642
yougoforward <908865817@qq.com>
2020-05-28 16:51:03 +0800

    gs5

--- experiments/segmentation/scripts/psp_att_noguide_res101_pcontext.sh
+++ experiments/segmentation/scripts/psp_att_noguide_res101_pcontext.sh
@@ -2,14 +2,14 @@
 # train
 python train.py --dataset pcontext \
     --model psp_att_noguide --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname psp_att_noguide_res50_pcontext
+    --backbone resnet101 --checkname psp_att_noguide_res101_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
     --model psp_att_noguide --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/psp_att_noguide/psp_att_noguide_res50_pcontext/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet101 --resume runs/pcontext/psp_att_noguide/psp_att_noguide_res101_pcontext/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
     --model psp_att_noguide --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/psp_att_noguide/psp_att_noguide_res50_pcontext/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume runs/pcontext/psp_att_noguide/psp_att_noguide_res101_pcontext/checkpoint.pth.tar --split val --mode testval --ms
