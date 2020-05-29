52e32333d7a1cba408c60b448c4bbdf70cdd2978
yougoforward <908865817@qq.com>
2020-05-29 21:34:41 +0800

    gs5

--- experiments/segmentation/scripts/gsnet5_res101_pcontext.sh
+++ experiments/segmentation/scripts/gsnet5_res101_pcontext.sh
@@ -1,8 +1,8 @@
 # !/usr/bin/env bash
-# train
-python train.py --dataset pcontext \
-    --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname gsnet5_res101_pcontext --lr 0.001
+# # train
+# python train.py --dataset pcontext \
+#     --model gsnet5 --aux --dilated --base-size 520 --crop-size 520 \
+#     --backbone resnet101 --checkname gsnet5_res101_pcontext --lr 0.001
 
 #test [single-scale]
 python test.py --dataset pcontext \
